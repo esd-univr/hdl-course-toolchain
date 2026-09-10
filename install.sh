20d1bdf1492ba37a27f0edbe1fe2e481ddb7ae88
Galfurian <enrico.fraccaroli@univr.it>
2026-09-10 10:55:20 +0200

    release: v1.4.0

--- install.sh
+++ install.sh
@@ -20,8 +20,8 @@
 set -u
 
 # --- pinned by scripts/release.sh when a release is cut -------------------
-VERSION="v0.0.0-dev"
-SHA256="ed32305739d91a2fc452805509e2b4c6272527bc06d7910bf55f37a4a24be699"
+VERSION="v1.4.0"
+SHA256="e542d6083b9e6cf9ebd49083bed0a63d8d6ad778a484ace0fcdeb5cbe310ebd5"
 # ------------------------------------------------------------------------
 
 REPO="esd-univr/hdl-course-toolchain"
